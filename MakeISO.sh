04b720cb755a9eab724d0de5b2e087aab1684eb3
Andrew Maney <xxxmemescoepxxx@gmail.com>
2025-05-01 00:03:58 -0400

    Create python compiler script

--- MakeISO.sh
+++ MakeISO.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
-make clean
-make -B
-
-BUILD_EXITCODE=$?
-
-if [ $BUILD_EXITCODE -ne 0 ]; then
-      echo "Build failed, exiting with status $BUILD_EXITCODE..."
-      exit $BUILD_EXITCODE
-fi
+set -e
 
 # Create a directory which will be our ISO root and copy the relevant files into it.
+echo "Creating ISO root directories..."
 mkdir -p ISORoot
 mkdir -p ISORoot/UI
 mkdir -p ISORoot/Boot/Limine
+
+echo "Copying files to ISO root directories..."
 cp -v bin/BorealOS ISORoot/Boot/
 cp -v BootloaderWallpaper.png ISORoot/UI/
 cp -v Limine.conf limine/limine-bios.sys limine/limine-bios-cd.bin \
       limine/limine-uefi-cd.bin ISORoot/Boot/Limine/
 
 # Create the EFI boot tree and copy Limine's EFI executables over.
+echo "Creating EFI boot tree and copying limine EFI executable to ISO root directories..."
 mkdir -p ISORoot/EFI/BOOT
 cp -v limine/BOOTX64.EFI ISORoot/EFI/BOOT/
 cp -v limine/BOOTIA32.EFI ISORoot/EFI/BOOT/
 
 # Create the bootable ISO.
+echo "Creating ISO image..."
 xorriso -as mkisofs -R -r -J -b Boot/Limine/limine-bios-cd.bin \
         -no-emul-boot -boot-load-size 4 -boot-info-table -hfsplus \
         -apm-block-size 2048 --efi-boot Boot/Limine/limine-uefi-cd.bin \
@@ -31,4 +28,6 @@ xorriso -as mkisofs -R -r -J -b Boot/Limine/limine-bios-cd.bin \
         ISORoot -o BorealOS.iso
 
 # Install Limine stage 1 and 2 for legacy BIOS boot.
+echo
+echo "Installing BIOS/UEFI limine in ISO image..."
 ./limine/limine bios-install BorealOS.iso
