8af113a48cf5d313781de1b9027967110c161952
Andrew Maney <xxxmemescoepxxx@gmail.com>
2026-02-27 00:31:28 -0500

    Restart APIC

--- src/kernel/arch/x86_64/Interrupts/APIC.cpp
+++ src/kernel/arch/x86_64/Interrupts/APIC.cpp
@@ -15,52 +15,35 @@ namespace Interrupts {
             PANIC("This system does not support APIC!");
         }
 
-        // Get the MADT table and the LAPIC address. Since it's MMIO, we need to map the pages correctly
-        _madt = (Core::ACPI::MADT*)_acpi->GetTable("APIC");
-        
-        if (!_madt) {
-            PANIC("Failed to find APIC MADT table!");
-        }
-
-        LOG_DEBUG("APIC MADT table is at address %p.", _madt);
-        uint64_t LAPICAddressMADT = (uint64_t)_madt->localAPICAddr;
-
         // Extract the physical APIC base address (bits 12 to 35)
         uint64_t APICBaseIA32 = _cpu->ReadMSR(MSR_IA32_APIC_BASE);
-        uint64_t LAPICBaseAddr = APICBaseIA32 & 0xFFFFFFFFFFFFF000ULL;
-        if (LAPICAddressMADT != LAPICBaseAddr) LOG_WARNING("MADT LAPIC address %p and MSR LAPIC address %p do not match!", LAPICAddressMADT, LAPICBaseAddr);
+        uint64_t MMIOLAPICPhysAddr = APICBaseIA32 & 0xFFFFFFFFFFFFF000ULL;
+        uint64_t MMIOLAPICVirtAddr = HIGHER_HALF(MMIOLAPICPhysAddr);
+        MMIOLAPICAddr = reinterpret_cast<volatile uint32_t*>(MMIOLAPICVirtAddr);
 
         // Check if the system supports x2APIC (bit 10) and that APIC is globally enabled (bit 11)
         if (APICBaseIA32 & (1ULL << 10)) PANIC("x2APIC mode is enabled, MMIO is not possible!");
         if ((APICBaseIA32 & (1ULL << 11)) == 0) PANIC("APIC is globally disabled!");
 
         // Map the APIC MMIO region
+        if (MMIOLAPICVirtAddr % Architecture::KernelPageSize != 0) PANIC("LAPIC base address is not page aligned!");
         _paging->MapPage(
-            LAPICBaseAddr + APIC_HIGHER_HALF_OFFSET,
-            LAPICBaseAddr,
+            MMIOLAPICVirtAddr,
+            MMIOLAPICPhysAddr,
             Memory::PageFlags::ReadWrite | Memory::PageFlags::NoExecute | Memory::PageFlags::CacheDisable
         );
 
-        MMIOLAPICAddr = reinterpret_cast<volatile uint32_t*>(LAPICBaseAddr + APIC_HIGHER_HALF_OFFSET);
-        LOG_DEBUG("Local APIC address is %p (physical address is %p).", MMIOLAPICAddr, LAPICBaseAddr);
+        LOG_DEBUG("MMIO LAPIC address is %p.", MMIOLAPICAddr);
+
+        // Find the MADT
+        _madt = (Core::ACPI::MADT*)_acpi->GetTable("APIC");
+        if (!_madt) PANIC("Failed to find the MADT!");
 
         // The 8259 PIC chip MUST be disabled before APIC can be used
-        // Note that if this APIC init fails, the PIC will be left disabled!
         if (_madt->flags & 1) LOG_WARNING("One or more legacy PIC chips were detected, they must be disabled for APIC to work properly.");
         _pic->Disable();
 
-        // Now that the PIC is disabled, we need to configure the spurious interrupt vector for the LAPIC. I will use the same offset as the standard PIC, which is 32
-        MMIOLAPICAddr[LAPIC_SVR_OFFSET / 4] = SIV_OFFSET | SVR_ENABLE_BIT;
-        if ((MMIOLAPICAddr[LAPIC_SVR_OFFSET / 4] & 0xFF) != SIV_OFFSET) PANIC("Failed to set spurious interrupt vector!");
-        LOG_DEBUG("LAPIC spurious interrupt vector is now %p, IRQ numbers will now start from %u8.", SIV_OFFSET, SIV_OFFSET);
-        
-        // Clear the task priority register
-        MMIOLAPICAddr[LAPIC_TPR_OFFSET / 4] = 0;
-
-        // Mask unused LVT entries
-        MMIOLAPICAddr[LVT_TIMER_OFFSET / 4] = LVT_MASK_BIT;
-        MMIOLAPICAddr[LVT_LINT0_OFFSET / 4] = LVT_MASK_BIT;
-        MMIOLAPICAddr[LVT_LINT1_OFFSET / 4] = LVT_MASK_BIT;
-        MMIOLAPICAddr[LVT_ERROR_OFFSET / 4] = LVT_MASK_BIT;
+        uint32_t LAPICID = MMIOLAPICAddr[0x20 / 4];
+        LOG_DEBUG("LAPIC ID: %u32", LAPICID);
     }
 }
--- src/kernel/arch/x86_64/Interrupts/APIC.h
+++ src/kernel/arch/x86_64/Interrupts/APIC.h
@@ -15,17 +15,7 @@ namespace Interrupts {
             explicit APIC(Core::ACPI* acpi, Core::CPU* cpu, PIC* pic, Memory::Paging* paging);
             void LAPIC_Spurious_Handler();
             void Initialize();
-            static constexpr uintptr_t APIC_HIGHER_HALF_OFFSET = 0xF8F8EFFE000;
-            static constexpr uint32_t SVR_ENABLE_BIT = 1 << 8;
-            static constexpr uint32_t LVT_MASK_BIT = 1 << 16;
-            static constexpr uint16_t LVT_TIMER_OFFSET = 0x320;
-            static constexpr uint16_t LVT_LINT0_OFFSET = 0x350;
-            static constexpr uint16_t LVT_LINT1_OFFSET = 0x360;
-            static constexpr uint16_t LVT_ERROR_OFFSET = 0x370;
             static constexpr uint8_t MSR_IA32_APIC_BASE = 0x1B;
-            static constexpr uint8_t LAPIC_TPR_OFFSET = 0x80;
-            static constexpr uint8_t LAPIC_SVR_OFFSET = 0xF0;
-            static constexpr uint8_t SIV_OFFSET = 0x20;
 
         private:
             Memory::Paging* _paging;
